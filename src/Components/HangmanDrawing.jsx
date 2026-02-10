a400ec944d59c1b4a9420555681e5c8c233f6ebd
itsanup1 <patilanup7748@gmail.com>
2026-02-10 17:14:12 +0530

    Changed Hangman Drawing design

--- src/Components/HangmanDrawing.jsx
+++ src/Components/HangmanDrawing.jsx
@@ -6,22 +6,22 @@ export function HangmanDrawing() {
       <div className="absolute top-0 right-[-124px] h-14 w-2 bg-black" />
       
       {/*Head*/}
-      <div className="absolute rounded-full border-6 h-22 w-22 right-[-164px] top-14"/>
+      <div className="absolute rounded-full border-12 h-20 w-20 right-[-160px] top-14"/>
       
       {/*Torso*/}
-      <div className="h-[160px] w-[6px] bg-black absolute right-[-124px] top-36"/>
+      <div className="h-36 w-3 bg-black absolute right-[-124px] top-32 rounded"/>
       
       {/*Left Hand*/}
-      <div className="absolute  h-24 rotate-58 w-[6px] bg-black -right-21 top-38" />
+      <div className="absolute  h-26 rotate-128 w-3 bg-black -right-20 top-28 rounded" />
       
       {/*Right Hand*/}
-      <div className="absolute  h-24 -rotate-58 w-[6px] bg-black -right-41 top-38" />
+      <div className="absolute  h-26 -rotate-128 w-3 bg-black -right-42 top-28 rounded" />
       
       {/*Left Leg*/}
-      <div className="absolute  h-24 rotate-58 w-[6px] bg-black -right-21 top-70" />
+      <div className="absolute  h-26 rotate-58 w-3 bg-black -right-20 top-60 rounded " />
       
       {/*Right Leg*/}
-      <div className="absolute  h-24 -rotate-58 w-[6px] bg-black -right-41 top-70" />
+      <div className="absolute  h-26 -rotate-58 w-3 bg-black -right-42 top-60 rounded" />
       
       {/* Top Bar */}
       <div className="h-2 w-[250px] ml-[121px] bg-black" />
