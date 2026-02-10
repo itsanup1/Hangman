3b6da6c7226dc07458c517e5df93d00f47bb6d82
itsanup1 <patilanup7748@gmail.com>
2026-02-10 17:23:36 +0530

    Fixed the size of drawing and angles

--- src/Components/HangmanDrawing.jsx
+++ src/Components/HangmanDrawing.jsx
@@ -9,19 +9,19 @@ export function HangmanDrawing() {
       <div className="absolute rounded-full border-12 h-20 w-20 right-[-160px] top-14"/>
       
       {/*Torso*/}
-      <div className="h-36 w-3 bg-black absolute right-[-124px] top-32 rounded"/>
+      <div className="h-36 w-3 bg-black absolute right-[-124px] top-32 rounded-full"/>
       
       {/*Left Hand*/}
-      <div className="absolute  h-26 rotate-128 w-3 bg-black -right-20 top-28 rounded" />
+      <div className="absolute  h-26 rotate-128 w-3 bg-black -right-22 top-24 rounded-full" />
       
       {/*Right Hand*/}
-      <div className="absolute  h-26 -rotate-128 w-3 bg-black -right-42 top-28 rounded" />
+      <div className="absolute  h-26 -rotate-128 w-3 bg-black -right-40 top-24 rounded-full" />
       
       {/*Left Leg*/}
-      <div className="absolute  h-26 rotate-58 w-3 bg-black -right-20 top-60 rounded " />
+      <div className="absolute  h-26 rotate-48 w-3 bg-black -right-22 top-62  rounded-full" />
       
       {/*Right Leg*/}
-      <div className="absolute  h-26 -rotate-58 w-3 bg-black -right-42 top-60 rounded" />
+      <div className="absolute  h-26 -rotate-48 w-3 bg-black -right-40 top-62 rounded-full" />
       
       {/* Top Bar */}
       <div className="h-2 w-[250px] ml-[121px] bg-black" />
